d60b25f6e43044b6025bb2d171b00744f72753d3
FOW <56652199+FOWMind@users.noreply.github.com>
2021-02-27 02:23:15 -0300

    uploading

---
+++ pricing-component-with-toggle/prices.js
@@ -0,0 +1,34 @@
+export default {
+    "prices": [
+        {
+            "name": "Basic",
+            "monthlyPrice": "$19.99",
+            "anuallyPrice": "$199.99",
+            "features": {
+                "storage": "500 GB",
+                "users": 2,
+                "sendUp": "3 GB"
+            }
+        },
+        {
+            "name": "Professional",
+            "monthlyPrice": "$24.99",
+            "anuallyPrice": "$249.99",
+            "features": {
+                "storage": "1 TB",
+                "users": 5,
+                "sendUp": "10 GB"
+            }
+        },
+        {
+            "name": "Master",
+            "monthlyPrice": "$39.99",
+            "anuallyPrice": "$399.99",
+            "features": {
+                "storage": "2 TB",
+                "users": 10,
+                "sendUp": "20 GB"
+            }
+        }
+    ]
+}
